3a489e7368556cedf8c342ca16764c6d1fea48b2
Арина Новгородова <arrrishanov@gmail.com>
2025-08-05 19:11:33 +0300

    add sort_by_category function

--- src/views.py
+++ src/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from src.utils import reading_excel_file
 
 
 def sort_by_date(input_day, period="M"):
@@ -15,3 +16,16 @@ def sort_by_date(input_day, period="M"):
         start = datetime.min
 
     return start.date(), date.date()
+
+def sort_by_category(data_dict, card_number, category):
+    """Функция, которая фильтрует операции по категории трат"""
+    sorted_transactions = []
+    for i in data_dict:
+        if i["Номер карты"] == f"*{str(card_number)[-4:]}":
+            sorted_transactions.append(i)
+
+    category_sorted = []
+    for tr in sorted_transactions:
+        if tr["Категория"] == category:
+            category_sorted.append(tr)
+    return category_sorted
