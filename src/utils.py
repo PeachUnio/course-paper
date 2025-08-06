195adf2d9c2743bd40c674c490646e1d05242562
Арина Новгородова <arrrishanov@gmail.com>
2025-08-06 13:22:11 +0300

    add load_users_settings function

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 import pandas as pd
 
 
@@ -11,3 +14,15 @@ def reading_excel_file(excel_path):
             return "Путь не найден"
     except Exception as e:
         return f"Поизошла ошибка {e}"
+
+
+def load_users_settings():
+    """Функция, которая загружает настройки пользователя из JSON-файла"""
+    try:
+        dir_path = Path(__file__).parent.parent
+        settings_path = dir_path / "user_settings.json"
+
+        with open(settings_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except Exception:
+        return f"Поизошла ошибка"
