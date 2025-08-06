1585444493a219278ec22c6e76ce853fcf0fa15d
Арина Новгородова <arrrishanov@gmail.com>
2025-08-06 18:10:45 +0300

    add tests for checking_exchange_rate function

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,9 @@
+import json
 import unittest
 from datetime import date
+from unittest.mock import Mock, patch
 
-from src.views import sort_by_date, sort_by_category
+from src.views import checking_exchange_rate, sort_by_date
 
 
 class TestSortByDate(unittest.TestCase):
@@ -20,3 +22,35 @@ class TestSortByDate(unittest.TestCase):
         start, end = sort_by_date("13.10.2022", "Y")
         self.assertEqual(start, date(2022, 1, 1))
         self.assertEqual(end, date(2022, 10, 13))
+
+
+class TestCheckingExchangeRate(unittest.TestCase):
+
+    def test_checking_exchange_rate(self):
+        with (
+            patch.dict("os.environ", {"API_CURRENCIES": "test_key"}),
+            patch("src.utils.load_users_settings") as mock_load,
+            patch("requests.get") as mock_get,
+        ):
+
+            mock_load.return_value = {"user_currencies": ["USD", "EUR"]}
+            mock_response = Mock()
+            mock_response.json.return_value = {"success": True, "rates": {"USD": 0.013, "EUR": 0.011}}
+            mock_response.raise_for_status.return_value = None
+            mock_get.return_value = mock_response
+
+            result = checking_exchange_rate()
+            expected_result = json.dumps(
+                [{"currency": "USD", "rate": 76.92}, {"currency": "EUR", "rate": 90.91}], indent=2
+            )
+
+            assert result == expected_result
+            mock_get.assert_called_once()
+
+    def test_checking_exchange_rate_error(self):
+        with patch.dict("os.environ", {"API_CURRENCIES": ""}), patch("src.utils.load_users_settings") as mock_load:
+
+            mock_load.return_value = {"user_currencies": ["USD", "EUR"]}
+            result = checking_exchange_rate()
+
+            assert result == "API ключ не найден"
