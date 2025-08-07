a849ff91024e92f7d8b52fa423e37abe8240a7db
Арина Новгородова <arrrishanov@gmail.com>
2025-08-07 22:22:31 +0300

    add spending_by_category function

--- src/reports.py
+++ src/reports.py
@@ -1,4 +1,7 @@
 from functools import wraps
+from datetime import datetime
+import json
+from src.utils import reading_excel_file
 
 
 def write_reports(filename=None):
@@ -18,3 +21,28 @@ def write_reports(filename=None):
         return inner
     return wrapper
 
+def spending_by_category(transactions, card_number, category, date=None):
+    """Функция возвращает траты по заданной категории за последние три месяца (от переданной даты)"""
+
+    end_date = datetime.strptime(date, "%d.%m.%Y") if date is not None else datetime.now()
+    start_date = end_date.replace(month=end_date.month - 3)
+
+    try:
+        sorted_transactions = [
+            t
+            for t in transactions
+            if (
+                t["Номер карты"] == f"*{str(card_number)[-4:]}"
+                and t["Статус"] == "OK"
+                and t["Сумма операции"] < 0
+                and start_date <= datetime.strptime(t["Дата операции"].split()[0], "%d.%m.%Y") <= end_date
+                and t["Категория"] == category
+            )
+        ]
+        if sorted_transactions:
+            return json.dumps(sorted_transactions, ensure_ascii=False, indent=2, default=str)
+        else:
+            return "Не найдено трат по заданным условиям"
+
+    except Exception as e:
+        return f"Произошла ошибка {e}"
